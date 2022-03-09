fdc7462129feb587067fd6a69122ec8bb780bdef
lytruongan613 <lytruongan613@gmail.com>
2022-03-09 11:37:37 +0700

    fix error loading. move out login login

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import  betterSqlite  from "better-sqlite3";
 const { app, BrowserWindow, dialog, ipcMain, session } = require('electron');
 const path = require('path');
 const os = require('os')
-
+import bettersqlite3 from "better-sqlite3";
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   // eslint-disable-line global-require
@@ -33,20 +32,21 @@ const createWindow = () => {
   console.log(MAIN_WINDOW_WEBPACK_ENTRY);
   console.log('====================================');
   // and load the index.html of the app.
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+  createDataBase();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(async () => {
-  // await session.defaultSession.loadExtension(reactDevToolsPath)
-  //Extension react custom add
-});
-//move to the end file
-// app.on('ready', createWindow);
+// app.whenReady().then(async () => {
+  // load react dev tool
+//   await session.defaultSession.loadExtension(reactDevToolsPath)
+// });
+app.on('ready', createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -57,62 +57,44 @@ app.on('window-all-closed', () => {
   }
 });
 
-// app.on('activate', () => {
-//   // On OS X it's common to re-create a window in the app when the
-//   // dock icon is clicked and there are no other windows open.
-//   if (BrowserWindow.getAllWindows().length === 0) {
-//     createWindow();
-//   }
-// });
-
-app
-  .whenReady()
-  .then(() => {
-    // ipcMain.handle('searchDb',getAllCustomer);
-    //ipcMain.handle('openFile', handleFileOpen)
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
-    createDataBase();
-    app.on('activate', () => {
-      // On macOS it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (mainWindow === null) createWindow();
-    });
-  })
-  .catch(console.log);
+  }
+});
 
+
+  function createDataBase() {
+    /**
+     * starting connection
+     */
+    let database;
+    let databasePath = 'src/database/test.db';
+  
+    try {
+      database = bettersqlite3(databasePath);
+    } catch(err) {
+      database = new bettersqlite3(databasePath);
+    }
+    /**
+     * creating table if not exists in the db
+     */
+    const sqlScript = `CREATE TABLE IF NOT EXISTS customer(
+      id TEXT PRIMARY KEY,
+      password TEXT NOT NULL);`;
+  
+    /**
+     * run script
+     */
+    database.prepare(sqlScript).run();
+  
+    /**
+     * close connection
+     */
+    database.close();
+  
+  }
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
-
-
-//an edit 
-function createDataBase() {
-  /**
-   * starting connection
-   */
-  let database;
-  let databasePath = 'src/database/test.db';
-
-  try {
-    database = betterSqlite(databasePath);
-  } catch(err) {
-    database = new betterSqlite(databasePath);
-  }
-  /**
-   * creating table if not exists in the db
-   */
-  const sqlScript = `CREATE TABLE IF NOT EXISTS customer(
-    id TEXT PRIMARY KEY,
-    password TEXT NOT NULL);`;
-
-  /**
-   * run script
-   */
-  database.prepare(sqlScript).run();
-
-  /**
-   * close connection
-   */
-  database.close();
-
-}
-//an edit 
--- src/renderer.js
+++ src/renderer.js
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom';
 
 import "./frontend/assets/resource/css/main.css";
 import "./frontend/assets/resource/css/main_sp.css";
-import "./frontend/assets/resource/css/slick.css";
+// import "./frontend/assets/resource/css/slick.css";
 
 import App from './frontend/App.jsx';
-console.log('Loaded React.');
+console.log('====================================');
+console.log("run react app");
+console.log('====================================');
 ReactDOM.render(<App></App>, document.getElementById('root'));
